0f3d024f89101ef2e90450e8b21a2d4c35cf0379
kekewong <tjunmeng@gmail.com>
2023-11-14 23:57:36 +0800

    imported morgan and helmet as middleware

--- tj-learn-react-backend/ExpressBackend/index.ts
+++ tj-learn-react-backend/ExpressBackend/index.ts
@@ -1,9 +1,13 @@
 import express, { Express, Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
 
 dotenv.config();
-
 const app: Application = express();
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("tiny"));
 const port = process.env.PORT || 8000;
 
 app.get("/", (req: Request, res: Response) => {
